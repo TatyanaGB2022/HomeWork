807dbbd6588e3146ef4404cbd66f013c3c50438c
Tatyana <tatyana.kurii@mail.ru>
2023-01-24 12:52:06 +0600

    Job4

---
+++ Job4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int startNumber = 1;
+
+while (startNumber <= a)
+{
+  if (startNumber % 2 == 0)
+    System.Console.Write(startNumber + ", ");
+  startNumber = startNumber + 1;
+}
