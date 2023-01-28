18d7f2a74aab662ab83a34281a9ce680886a2ee0
Tatyana <tatyana.kurii@mail.ru>
2023-01-28 22:43:35 +0600

    Seminar2 Job3

---
+++ Seminar2_Job3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет 
+
+System.Console.WriteLine("Введите число дня недели: ");
+string volue = Console.ReadLine();
+int dayOfWeek = Convert.ToInt32(volue);
+
+if (dayOfWeek >= 1 && dayOfWeek <= 5)
+{
+  System.Console.WriteLine("Ответ: Будний день.");
+}
+if (dayOfWeek == 6 || dayOfWeek == 7)
+{
+  System.Console.WriteLine("Ответ: Выходной день.");
+}
+if (dayOfWeek > 7)
+{
+  System.Console.WriteLine("Ошибка: В неделе всего 7 дней");
+}
