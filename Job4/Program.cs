f7e76f2b1f45115eaaf3055b745485612d0009fc
Tatyana <tatyana.kurii@mail.ru>
2023-01-30 19:54:43 +0600

    Job4

--- Job4/Program.cs
+++ Job4/Program.cs
@@ -3,13 +3,40 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-System.Console.WriteLine("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int startNumber = 1;
+//мое решение
 
-while (startNumber <= a)
+// System.Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int startNumber = 1;
+
+// while (startNumber <= a)
+// {
+//   if (startNumber % 2 == 0)
+//     System.Console.Write(startNumber + ", ");
+//   startNumber = startNumber + 1;
+// }
+
+//функция, определяющая четность числа
+bool isOrder(int Number)
 {
-  if (startNumber % 2 == 0)
-    System.Console.Write(startNumber + ", ");
-  startNumber = startNumber + 1;
+  int remainder = Number % 2;
+  return remainder == 0;
 }
+
+System.Console.WriteLine("Введите последнее число: ");
+string valueString = Console.ReadLine();
+int N = Convert.ToInt32(valueString);
+string Numbers = "";
+int count = 2; //начинаем с 2, потому что оно первое четное число
+string separator = "";
+while (count <= N)
+{
+  if (count > 2)
+  {
+    separator = ", ";
+  }
+  Numbers = Numbers + separator + count;
+  count += 2; //добавляем не 1, а 2 - будем идти по четным
+}
+
+System.Console.WriteLine($"Четные числа {Numbers}");
