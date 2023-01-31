7ca1da9c9f1a8ec89a3bf41b5ec5253fc211103f
Tatyana <tatyana.kurii@mail.ru>
2023-01-31 12:13:14 +0600

    Seminar2 Jod4

--- Seminar2_Job4/Program.cs
+++ Seminar2_Job4/Program.cs
@@ -1,15 +1,28 @@
 ﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
 // Не использовать строки для расчета.
 
-System.Console.WriteLine();
-int Number = new Random().Next(100, 1000);
-int NewNumber = 0;
+//мое решение
 
-int NumberWithoutSecDig()
+// System.Console.WriteLine();
+// int Number = new Random().Next(100, 1000);
+// int NewNumber = 0;
+
+// int NumberWithoutSecDig()
+// {
+//   NewNumber = Number / 100 * 10 + Number % 10;
+//   return NewNumber;
+// }
+
+// NumberWithoutSecDig();
+// System.Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}");
+
+int RandomInt()
 {
-  NewNumber = Number / 100 * 10 + Number % 10;
-  return NewNumber;
+  Random rnd = new Random(); // rnd сокращенно Random
+  return rnd.Next(100, 1000); //вернем 100 включительно, 1000 исключительно
 }
-
-NumberWithoutSecDig();
-System.Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}");
+int number = RandomInt();
+int rank1 = number % 10;
+int rank2 = number / 100;
+int result = rank2 * 10 + rank1;
+System.Console.WriteLine($"Изначальное число {number}, результат {result}");
