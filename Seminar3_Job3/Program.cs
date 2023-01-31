5121b0055acd088cc780b53b9c51540a877da2d2
Tatyana <tatyana.kurii@mail.ru>
2023-01-31 21:22:23 +0600

    Seminar3 Job3

---
+++ Seminar3_Job3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+  System.Console.WriteLine($"{message} ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+int N = Prompt("Введите число: ");
+int i = 1;
+while (i <= N)
+{
+  double result = Math.Pow(i, 3);
+  System.Console.WriteLine($"Третья степень числа {i} = " + result);
+  i++;
+}
