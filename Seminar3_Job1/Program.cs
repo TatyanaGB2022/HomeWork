58e01110f8c915493d8bb4d02c7274a552860445
Tatyana <tatyana.kurii@mail.ru>
2023-01-31 20:00:17 +0600

    Seminar3 Job1

---
+++ Seminar3_Job1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом. Не использовать строки
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Является палиндромом");
+    }
+    else
+    {
+        Console.Write($"{number} - Не является палиндромом");
+    }
+}
+else
+{
+    Console.Write($"{number} - не является пятизначным");
+}
