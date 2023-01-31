2322c7622918cab73a186cf81d25e2457e28abf1
Tatyana <tatyana.kurii@mail.ru>
2023-01-31 11:14:57 +0600

    Seminar2 Jod1

--- Seminar2_Job1/Program.cs
+++ Seminar2_Job1/Program.cs
@@ -5,14 +5,42 @@
 // 782 -> 8
 // 918 -> 1
 
-System.Console.WriteLine("Введите трехзначное число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-if (a > 99 && a < 1000)
+
+// мое решение
+
+// System.Console.WriteLine("Введите трехзначное число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a > 99 && a < 1000)
+// {
+//   int result = ((a / 10) % 10);
+//   System.Console.WriteLine("Вторая цифра числа равна " + result);
+// }
+// else
+// {
+//   System.Console.WriteLine("Число не трехзначное");
+// }
+
+int InputInt (string massage)
 {
-  int result = ((a / 10) % 10);
-  System.Console.WriteLine("Вторая цифра числа равна " + result);
+  System.Console.Write(massage + " > ");
+  string? InputValue = Console.ReadLine(); //вопрос означает, что InputValue может содержать ссылку в никуда. "?" можно убрать
+  int result = Convert.ToInt32(InputValue);
+  return result;
 }
-else
+
+bool Validate(int number) //функция проверки числа. Попадает число в диапазон или нет
 {
-  System.Console.WriteLine("Число не трехзначное");
+  if (number > 99 && number < 1000)
+  {
+    return true;
+  }
+  System.Console.WriteLine("Число не трехзначное. ");
+  return false;
+}
+int number = InputInt("Введите трехзначное число.");
+
+if (Validate(number))
+{
+  int LastRank = ((number / 10) % 10); // трехзначное делим на 10 и из двухзначного вытаскивает единицы
+  System.Console.WriteLine($"Вторая цифра числа {number} равна {LastRank}");
 }
