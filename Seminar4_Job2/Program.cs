37252e04dcdbf9ccb4c2135be21f876c19208343
Tatyana <tatyana.kurii@mail.ru>
2023-02-02 21:31:24 +0600

    Seminar4 Job2

---
+++ Seminar4_Job2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.Write("Введите число > ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Prompt(int number)
+{
+  int count = 0;
+  int result = 0;
+
+  while (number > 0)
+  {
+    count = number % 10;
+    result = result + count;
+    number = number / 10;
+  }
+  return result;
+}
+int prompt = Prompt(number);
+System.Console.WriteLine("Сумма цифр в числе " + prompt);
