20d9294e9d902b6d637b6bf8db10e940344de155
Tatyana <tatyana.kurii@mail.ru>
2023-02-08 15:31:35 +0600

    Job1 Job2

---
+++ Seminar6_Job1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько 
+// чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.Write("Введите колличество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+  System.Console.Write($"Введите число {i + 1}: ");
+  int x = Convert.ToInt32(Console.ReadLine());
+  if (x > 0)
+    count++;
+}
+System.Console.WriteLine($"Количество положительных чисел: {count}");
