072ebbaaaec97b997e91fe1588ea18ef5f27311d
Tatyana <tatyana.kurii@mail.ru>
2023-02-14 17:34:30 +0600

    Seminar9 Job1

---
+++ Seminar9_Job2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+System.Console.WriteLine("Введите число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите число N:");
+int numberN = int.Parse(Console.ReadLine());
+System.Console.WriteLine();
+
+
+void GapNumberSum(int numberM, int numberN, int sum)
+{
+  if (numberM > numberN)
+  {
+    System.Console.WriteLine($"{sum}");
+    return;
+  }
+  sum = sum + (numberM++);
+  GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
